fd6557edc9636ee1f4980971c3cea79d9ae2670a
bmackenzie <brian.e.mackenzie@outlook.com>
2021-04-15 17:48:22 -0500

    Card Preview
    Added functionality for previewing a larger version of a card on hover

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+
+    public GameObject canvas;
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+    private GameObject dropZone;
+    private GameObject startParent;
+    private Vector2 startPosition;
+
+
+    private void Awake()
+    {
+        canvas = GameObject.Find("Main Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if the object is being dragged, and moves the object to the mouse position
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(canvas.transform, true);
+        }    
+    }
+
+    //checks if object is colliding with something, if so make that collision the drop area
+    //needs to check for a specific collider if the game has multiple things with collisions
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    //set start position in case player drops the object somewhere they can't
+    public void StartDrag()
+    {
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    //set object transform to the dropzone if its over a collider, send it back to the start position if not
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        } 
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/DrawCards.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCards : MonoBehaviour
+{
+
+    public GameObject card1;
+    public GameObject card2;
+    public GameObject playerArea;
+    public GameObject enemyArea;
+
+    List<GameObject> cards = new List<GameObject>();
+
+    void Start()
+    {
+        cards.Add(card1);
+        cards.Add(card2);
+    }
+
+    public void Draw()
+    {
+        for(var i = 0; i < 5; i++)
+        {
+            GameObject playerCard = Instantiate(cards[Random.Range(0,cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            playerCard.transform.SetParent(playerArea.transform, false);
+
+            GameObject enemyCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0, 0, 0), Quaternion.identity);
+            enemyCard.transform.SetParent(enemyArea.transform, false);
+        }
+
+    }
+}
